bd35e7de5cd7e9385eb59ff0ee5b6a9729a114cc
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-12-02 18:41:45 -0300

    Adicionar testes para o repositório Coordenador e método de criação no Factory

---
+++ src/test/java/com/ufma/portal_egresso/repository/CoordenadorRepositoryTest.java
@@ -0,0 +1,126 @@
+package com.ufma.portal_egresso.repository;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.ufma.portal_egresso.model.Coordenador;
+import com.ufma.portal_egresso.model.Curso;
+import com.ufma.portal_egresso.repository.factory.Factory;
+
+
+@DataJpaTest
+public class CoordenadorRepositoryTest {
+
+    @Autowired
+    private CoordenadorRepository repository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+
+    private Long totalCoordenador;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        totalCoordenador = repository.count();
+    }
+
+    @Test
+    public void shouldSaveCoordenador() {
+
+        Curso curso = Factory.createCurso();
+
+        cursoRepository.save(curso);
+
+        Coordenador coordenador = Factory.createCoordenador();
+
+        coordenador.getCursos().add(curso);
+
+        repository.save(coordenador);
+
+        Assertions.assertNotNull(coordenador.getId_coordenador());
+        Assertions.assertEquals(totalCoordenador + 1, repository.count());
+
+        Optional<Coordenador> result = repository.findById(coordenador.getId_coordenador());
+
+        Assertions.assertTrue(result.isPresent());
+        Assertions.assertEquals(coordenador.getLogin(), result.get().getLogin());
+    }
+
+    @Test
+    public void shouldDeleteCoordenador(){
+
+        Curso curso = Factory.createCurso();
+
+        cursoRepository.save(curso);
+
+        Coordenador coordenador = Factory.createCoordenador();
+
+        coordenador.getCursos().add(curso);
+
+        repository.save(coordenador);
+
+        repository.delete(coordenador);
+
+        Assertions.assertEquals(totalCoordenador, repository.count());
+    }
+
+
+    @Test
+    public void shouldUpdateCoordenador(){
+
+        Curso curso = Factory.createCurso();
+
+        cursoRepository.save(curso);
+
+        Coordenador coordenador = Factory.createCoordenador();
+
+        coordenador.getCursos().add(curso);
+
+        repository.save(coordenador);
+
+        Assertions.assertNotNull(coordenador.getId_coordenador());
+        Assertions.assertEquals(totalCoordenador + 1, repository.count());
+
+        coordenador.setLogin("login");
+
+        repository.save(coordenador);
+
+        Optional<Coordenador> result = repository.findById(coordenador.getId_coordenador());
+
+        Assertions.assertTrue(result.isPresent());
+        Assertions.assertEquals(coordenador.getLogin(), result.get().getLogin());
+    }
+
+    @Test
+    public void shouldReturnNullWhenCoordenadorDoesNotExist(){
+            
+            Optional<Coordenador> result = repository.findById(0L);
+    
+            Assertions.assertTrue(result.isEmpty());
+    }
+
+
+    @Test
+    public void shouldReturnAllCoordenador(){
+            
+            Curso curso = Factory.createCurso();
+
+            cursoRepository.save(curso);
+
+            Coordenador coordenador = Factory.createCoordenador();
+
+            coordenador.getCursos().add(curso);
+
+            repository.save(coordenador);
+
+            Assertions.assertEquals(totalCoordenador+1, repository.count());
+    }
+
+}
