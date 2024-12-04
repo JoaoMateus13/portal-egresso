5920ac8f6ea036dc5893996335bee2c1025877a0
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-12-04 15:45:21 -0300

    Adicionar testes para o serviço Coordenador, incluindo métodos de login e gerenciamento de Egresso; corrigir exceção lançada em caso de falha no login

--- src/test/java/com/ufma/portal_egresso/service/CoordenadorServiceTests.java
+++ src/test/java/com/ufma/portal_egresso/service/CoordenadorServiceTests.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.lang.IllegalArgumentException;
+import java.lang.foreign.Linker.Option;
 
 import com.ufma.portal_egresso.factory.Factory;
 import com.ufma.portal_egresso.model.Coordenador;
 import com.ufma.portal_egresso.repository.CoordenadorRepository;
 import com.ufma.portal_egresso.repository.CursoRepository;
+import com.ufma.portal_egresso.model.Egresso;
 import com.ufma.portal_egresso.repository.EgressoRepository;
+
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @SpringBootTest
 public class CoordenadorServiceTests {
@@ -27,6 +32,8 @@ public class CoordenadorServiceTests {
     @Autowired
     private CoordenadorRepository coordenadorRepository;
 
+    private long totalEgressos;
+
     @BeforeEach
     void setUp() {
         // Cria e salva um coordenador para teste
@@ -34,13 +41,34 @@ public class CoordenadorServiceTests {
         coordenador.setLogin("test");
         coordenador.setSenha("test");
         coordenadorRepository.save(coordenador);
+
+        totalEgressos = egressoRepository.count();
+
+
     }
 
-    // @Test
-    // public void deveRetornarErro
+    @Test
+    public void loginSuccessShouldReturnTrue() {
+        Assertions.assertTrue(coordenadorService.efetuarLogin("test", "test"));
+    }
 
     @Test
-    public void deveEfectuarLogin() {
-        Assertions.assertTrue(coordenadorService.efetuarLogin("admin", "admin"));
+    public void loginFailurehouldReturnExeptionIllegalArgument() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+         coordenadorService.efetuarLogin("test", "wrong"));
+    }
+
+
+    @Test
+    public void shouldSaveEgresso() {
+        
+        Egresso egresso =  Factory.createEgresso();
+
+        Egresso egresso2 = coordenadorService.ManterEgresso(egresso);
+
+
+        Assertions.assertNotNull(egresso2);
+        Assertions.assertEquals(egresso.getNome(), egresso2.getNome());
+        Assertions.assertEquals(totalEgressos + 1, egressoRepository.count());
     }
 }
